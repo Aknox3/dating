5848cd9c1317d1db86233eaff78fd6892f9e22f2
Ashton Knox <aknox3@mail.greenriver.edu>
2018-02-03 00:01:25 -0500

    bugfixing

--- model/validInterests.php
+++ model/validInterests.php
@@ -7,4 +7,24 @@ require "validation.php";
 $errors = array();
 
 //valide interests
+$validIndoors = true;
+$validOutdoors = true;
+
+$indoorOptions = array("tv","movies","cooking","bgames","puzzles","reading","cards","vgames");
+$outdoorOptions = array("hiking","biking","swimming","collecting","walking","climbing");
+foreach ($outdoorInterests as $interest)
+{
+    if (!in_Array($outdoorOptions, $interest))
+    {
+        $validIndoors = false;
+    }
+}
+
+foreach ($indoorInterests as $interest)
+{
+    if (!in_Array($indoorOptions, $interest))
+    {
+        $validOutdoors = false;
+    }
+}
 $success = (sizeof($errors) == 0);
